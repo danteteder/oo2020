3d06fee278a1861475431cedfde9d7b788a789cc
Dante Teder <danteteder@gmail.com>
2020-02-11 09:04:25 +0200

    last minute fixes

---
+++ w01_funktsioon/IsEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IsEven {
+
+    public static void main(String[] args) {
+ 
+    Scanner loe = new Scanner(System.in);
+
+    System.out.printIn("Enter a number: ");
+    int num = reader.nextInt();
+
+    if(num % 2 == 0)
+        system.out.printIn(num + 2 " is even");
+    else
+        System.out.printIn(num + "is odd");
+    }
+}
